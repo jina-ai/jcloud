d5d5d1f80a6b4738d08db043cc5ae79c253c552b
Han Xiao <han.xiao@jina.ai>
2022-04-11 22:10:13 +0200

    chore: first commit

--- jcloud/flow.py
+++ jcloud/flow.py
@@ -14,6 +14,7 @@ logger = get_logger()
 
 pbar, pb_task = get_pbar('', total=5)
 
+
 class Status(str, Enum):
     SUBMITTED = 'SUBMITTED'
     STARTING = 'STARTING'
@@ -46,10 +47,10 @@ class CloudFlow:
         return f'{self.name}-{self.flow_id.split("-")[1]}.wolf.jina.ai'
 
     @property
-    def loop(self):
+    def _loop(self):
         return get_or_reuse_loop()
 
-    async def deploy(self):
+    async def _deploy(self):
 
         params = {}
         if self.name:
@@ -61,14 +62,14 @@ class CloudFlow:
             pbar.update(pb_task, advance=1, description='Submitting...')
 
             async with session.post(
-                    url=WOLF_API,
-                    data={'yaml': open(self.filepath)},
-                    params=params,
-                    headers=AUTH_HEADERS,
+                url=WOLF_API,
+                data={'yaml': open(self.filepath)},
+                params=params,
+                headers=AUTH_HEADERS,
             ) as response:
                 json_response = await response.json()
                 assert (
-                        response.status == HTTPStatus.CREATED
+                    response.status == HTTPStatus.CREATED
                 ), f'Got Invalid response status {response.status}, expected {HTTPStatus.CREATED}'
                 if self.name:
                     assert self.name in json_response['name']
@@ -77,7 +78,9 @@ class CloudFlow:
                 self.flow_id: str = json_response['id']
                 self.workspace: str = json_response['workspace']
 
-                logger.debug(f'POST /flows with flow_id {self.flow_id} & request_id {json_response["request_id"]}')
+                logger.debug(
+                    f'POST /flows with flow_id {self.flow_id} & request_id {json_response["request_id"]}'
+                )
 
                 self._c_logstream_task = asyncio.create_task(
                     CloudFlow.logstream({'request_id': json_response['request_id']})
@@ -91,10 +94,10 @@ class CloudFlow:
                 response.raise_for_status()
                 return await response.json()
 
-    async def fetch_until(
-            self,
-            intermediate: List[Status],
-            desired: Status = Status.ALIVE,
+    async def _fetch_until(
+        self,
+        intermediate: List[Status],
+        desired: Status = Status.ALIVE,
     ):
         wait_seconds = 0
         while wait_seconds < 600:
@@ -112,15 +115,15 @@ class CloudFlow:
                 await asyncio.sleep(1)
                 wait_seconds += 1
 
-    async def terminate(self):
+    async def _terminate(self):
         async with aiohttp.ClientSession() as session:
             async with session.delete(
-                    url=f'{WOLF_API}/{self.flow_id}',
-                    headers=AUTH_HEADERS,
+                url=f'{WOLF_API}/{self.flow_id}',
+                headers=AUTH_HEADERS,
             ) as response:
                 json_response = await response.json()
                 assert (
-                        response.status == HTTPStatus.ACCEPTED
+                    response.status == HTTPStatus.ACCEPTED
                 ), f'Got Invalid response status {response.status}, expected {HTTPStatus.ACCEPTED}'
                 self.t_logstream_task = asyncio.create_task(
                     CloudFlow.logstream(
@@ -149,7 +152,11 @@ class CloudFlow:
                                 log_dict: Dict = msg.json()
                                 if log_dict.get('status') == 'STREAMING':
                                     if _first_msg:
-                                        pbar.update(pb_task, description='Streaming...', advance=1)
+                                        pbar.update(
+                                            pb_task,
+                                            description='Streaming...',
+                                            advance=1,
+                                        )
                                         _first_msg = False
                                     logger.debug(log_dict['message'])
                     logger.debug(f'Disconnected from the logstream server ...')
@@ -166,37 +173,37 @@ class CloudFlow:
     async def __aenter__(self):
         with pbar:
             pbar.start_task(pb_task)
-            await self.deploy()
+            await self._deploy()
             pbar.update(pb_task, description='Starting...', advance=1)
-            self.gateway = await self.fetch_until(
+            self.gateway = await self._fetch_until(
                 intermediate=[Status.SUBMITTED, Status.STARTING],
                 desired=Status.ALIVE,
             )
             await self._c_logstream_task
             if self.enable_logstream:
-                self.logstream_task = self.loop.create_task(
+                self.logstream_task = self._loop.create_task(
                     CloudFlow.logstream(params={'flow_id': str(self.flow_id)})
                 )
             return self
 
     async def __aexit__(self, *args, **kwargs):
-        await self.terminate()
-        await self.fetch_until(
+        await self._terminate()
+        await self._fetch_until(
             intermediate=[Status.DELETING],
             desired=Status.DELETED,
         )
         await self.t_logstream_task
-        await CloudFlow.cancel_pending()
+        await CloudFlow._cancel_pending()
 
     @staticmethod
-    async def cancel_pending():
+    async def _cancel_pending():
         for task in asyncio.all_tasks():
             task.cancel()
             with suppress(asyncio.CancelledError, RuntimeError):
                 await task
 
     def __enter__(self):
-        return self.loop.run_until_complete(self.__aenter__())
+        return self._loop.run_until_complete(self.__aenter__())
 
     def __exit__(self, *args, **kwargs):
-        self.loop.run_until_complete(self.__aexit__(*args, **kwargs))
+        self._loop.run_until_complete(self.__aexit__(*args, **kwargs))

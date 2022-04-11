13a08e7befc3698d83503ba3e42fbbca3e9c9318
Han Xiao <han.xiao@jina.ai>
2022-04-11 11:16:20 +0200

    chore: first commit

---
+++ setup.py
@@ -0,0 +1,73 @@
+import sys
+from os import path
+
+from setuptools import find_packages
+from setuptools import setup
+
+if sys.version_info < (3, 7, 0):
+    raise OSError(f'Requires Python >=3.7, but yours is {sys.version}')
+
+try:
+    pkg_name = 'wolf'
+    libinfo_py = path.join(pkg_name, '__init__.py')
+    libinfo_content = open(libinfo_py, 'r', encoding='utf8').readlines()
+    version_line = [l.strip() for l in libinfo_content if l.startswith('__version__')][
+        0
+    ]
+    exec(version_line)  # gives __version__
+except FileNotFoundError:
+    __version__ = '0.0.0'
+
+try:
+    with open('README.md', encoding='utf8') as fp:
+        _long_description = fp.read()
+except FileNotFoundError:
+    _long_description = ''
+
+setup(
+    name=pkg_name,
+    packages=find_packages(),
+    version=__version__,
+    include_package_data=True,
+    description='The data structure for unstructured data',
+    author='Jina AI',
+    author_email='hello@jina.ai',
+    license='Apache 2.0',
+    url='https://github.com/jina-ai/wolf-cli',
+    download_url='https://github.com/jina-ai/wolf-cli/tags',
+    long_description=_long_description,
+    long_description_content_type='text/markdown',
+    zip_safe=False,
+    setup_requires=['setuptools>=18.0', 'wheel'],
+    install_requires=['numpy', 'rich>=12.0.0'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Unix Shell',
+        'Environment :: Console',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Database :: Database Engines/Servers',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+        'Topic :: Scientific/Engineering :: Image Recognition',
+        'Topic :: Multimedia :: Video',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    project_urls={
+        'Documentation': 'https://wolf-cli.jina.ai',
+        'Source': 'https://github.com/jina-ai/wolf-cli/',
+        'Tracker': 'https://github.com/jina-ai/wolf-cli/issues',
+    },
+    keywords='wolf-cli deep-learning data-structures cross-modal multi-modal unstructured-data nested-data neural-search',
+)

67f12816a3a084f72ac7840bedbd1c5b9a98cd59
Deepankar Mahapatro <deepankar.mahapatro@jina.ai>
2022-11-11 13:23:56 +0530

    test: custom name

---
+++ tests/integration/custom_labels/test_custom_labels.py
@@ -0,0 +1,28 @@
+import os
+
+import pytest
+from jina import Client, Document, DocumentArray
+
+from jcloud.flow import CloudFlow
+
+flows_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'flows')
+protocol = 'grpc'
+
+
+@pytest.mark.asyncio
+async def test_custom_labels(capsys):
+    labels_file = 'valid-labels.yml'
+
+    async with CloudFlow(path=os.path.join(flows_dir, labels_file)) as flow:
+        assert flow.endpoints != {}
+        assert 'gateway' in flow.endpoints
+        gateway = flow.endpoints['gateway']
+        assert gateway.startswith(f'{protocol}s://')
+
+        da = Client(host=gateway).post(
+            on='/',
+            inputs=DocumentArray(Document(text=f'text-{i}') for i in range(50)),
+        )
+        assert len(da.texts) == 50
+
+        # TODO: check that the labels are actually set by searching for them

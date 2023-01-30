1a90eabfe08e18ab299e927ca70f537db045374d
Nan Wang <nan.wang@jina.ai>
2023-01-30 14:50:02 +0800

    feat: minor update

--- jcloud/parsers/normalize.py
+++ jcloud/parsers/normalize.py
@@ -9,19 +9,19 @@ def set_normalize_parser(parser=None):
     parser.add_argument(
         'path',
         type=Path,
-        help='The local path to a Jina flow project directory or yml file.',
+        help='The local path to a Jina Flow project directory or yml file.',
     )
     parser.add_argument(
         '-o',
         '--output',
         type=Path,
-        help='The output path to the normalized Jina flow yml file.',
+        help='The output path to the normalized Jina Flow yml file.',
     )
     parser.add_argument(
         '-v',
         '--verbose',
         action='store_true',
-        help='Increase verbosity',
+        help='Increase verbosity.',
     )
     parser.set_defaults(verbose=False)
     return parser

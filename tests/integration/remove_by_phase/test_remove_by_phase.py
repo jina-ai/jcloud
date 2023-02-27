08b853197396eb338383164b9d247557b42ec76b
Nikolas Pitsillos <npitsillos@gmail.com>
2023-02-27 10:28:04 +0000

    test: integration test with args

--- tests/integration/remove_by_phase/test_remove_by_phase.py
+++ tests/integration/remove_by_phase/test_remove_by_phase.py
@@ -16,6 +16,9 @@ from tests.integration.remove_multiple.test_multi_flows_removal import (
     get_serving_flows,
 )
 
+NUM_FLOWS_TO_DEPLOY = 5
+NUM_FLOWS_TO_PAUSE = 2
+
 logger = get_logger("Test Logger")
 
 flows_dir = os.path.join(
@@ -59,4 +62,62 @@ def test_remove_flows_by_phase(mock_ask):
     remove(args)
     paused_flows = asyncio.run(get_paused_flows())
 
+    assert any([flow_id in paused_flows for flow_id in added_flows]) == False
+
+
+@patch('rich.prompt.Confirm.ask', return_value=True)
+def test_remove_flows_by_phase_and_list_with_empty_intersection(mock_ask):
+    initial_owned_flows = asyncio.run(get_serving_flows())
+    logger.info(f'Initial owned flows: {len(initial_owned_flows)}')
+    added_flows = set()
+    for _ in range(NUM_FLOWS_TO_DEPLOY):
+        added_flows.add(
+            asyncio.run(
+                _simplified_deploy(CloudFlow(path=os.path.join(flows_dir, flow)))
+            )
+        )
+    logger.info(f'Deploying {NUM_FLOWS_TO_DEPLOY} new flows...')
+    owned_flows_after_add = asyncio.run(get_serving_flows())
+    logger.info(f'New Flow added: {added_flows}')
+    logger.info(f'Owned flows after new deployments: {len(owned_flows_after_add)}')
+    assert added_flows.issubset(owned_flows_after_add)
+    for flow_id in added_flows:
+        asyncio.run(CloudFlow(flow_id=flow_id).pause())
+    logger.info(f'Removing new flows by phase and list...')
+    args = Namespace()
+    args.phase = 'Paused'
+    args.flows = added_flows[NUM_FLOWS_TO_PAUSE:]
+    remove(args)
+    paused_flows = asyncio.run(get_paused_flows())
+    owned_flows_after_add = asyncio.run(get_serving_flows)
+    assert any([flow_id in owned_flows_after_add for flow_id in added_flows]) == False
+    assert any([flow_id in paused_flows for flow_id in added_flows]) == False
+
+
+@patch('rich.prompt.Confirm.ask', return_value=True)
+def test_remove_flows_by_phase_and_list_with_non_empty_intersection(mock_ask):
+    initial_owned_flows = asyncio.run(get_serving_flows())
+    logger.info(f'Initial owned flows: {len(initial_owned_flows)}')
+    added_flows = set()
+    for _ in range(NUM_FLOWS_TO_DEPLOY):
+        added_flows.add(
+            asyncio.run(
+                _simplified_deploy(CloudFlow(path=os.path.join(flows_dir, flow)))
+            )
+        )
+    logger.info(f'Deploying {NUM_FLOWS_TO_DEPLOY} new flows...')
+    owned_flows_after_add = asyncio.run(get_serving_flows())
+    logger.info(f'New Flow added: {added_flows}')
+    logger.info(f'Owned flows after new deployments: {len(owned_flows_after_add)}')
+    assert added_flows.issubset(owned_flows_after_add)
+    for flow_id in added_flows:
+        asyncio.run(CloudFlow(flow_id=flow_id).pause())
+    logger.info(f'Removing new flows by phase and list...')
+    args = Namespace()
+    args.phase = 'Paused'
+    args.flows = added_flows[NUM_FLOWS_TO_PAUSE - 1 :]
+    remove(args)
+    paused_flows = asyncio.run(get_paused_flows())
+    owned_flows_after_add = asyncio.run(get_serving_flows())
+    assert any([flow_id in owned_flows_after_add for flow_id in added_flows]) == False
     assert any([flow_id in paused_flows for flow_id in added_flows]) == False

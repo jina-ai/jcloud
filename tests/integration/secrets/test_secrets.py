31ba1a8852305e107910c28b5852053d4ce08908
Nikos Pitsillos <npitsillos@gmail.com>
2023-07-03 14:59:02 +0100

    fix: secret integration test

--- tests/integration/secrets/test_secrets.py
+++ tests/integration/secrets/test_secrets.py
@@ -13,23 +13,19 @@ flow_file = 'http-flow.yml'
 def test_create_secret():
     with CloudFlow(path=os.path.join(flows_dir, flow_file)) as flow:
 
-        secret_response = asyncio.run(
-            flow.create_secret('mysecret', {'env1': {'test': 'value'}})
-        )
+        secret_response = asyncio.run(flow.create_secret('mysecret', {'env1': 'value'}))
         assert 'name' in secret_response
 
         secret = asyncio.run(flow.get_resource(Resources.Secret, 'mysecret'))
 
         assert secret['name'] == 'mysecret'
-        assert secret['data'] == {'test': 'value'}
+        assert secret['data'] == {'env1': 'value'}
 
-        asyncio.run(flow.update_secret('mysecret', {'key': 'test2'}))
+        asyncio.run(flow.update_secret('mysecret', {'env1': 'value2'}))
 
         secret = asyncio.run(flow.get_resource(Resources.Secret, 'mysecret'))
 
         assert secret['name'] == 'mysecret'
-        assert secret['data'] == {'key': 'test2'}
+        assert secret['data'] == {'env1': 'value2'}
 
         asyncio.run(flow.delete_resource(Resources.Secret, 'mysecret'))
-
-        assert f'{flow_file.split(".")[0]}-mysecret.yml' in os.listdir(flows_dir)

4329d7ef51986364a42a1fd32860d09ec90b2ad7
Nikos Pitsillos <npitsillos@gmail.com>
2023-07-11 16:13:17 +0100

    test: update jina new test

--- tests/integration/test_jina_new.py
+++ tests/integration/test_jina_new.py
@@ -1,20 +1,30 @@
 import os
+import pip
 import shutil
 import subprocess
 
 import pytest
-from jina import Client, DocumentArray
 
 from jcloud.flow import CloudFlow
+from jina import Client
 
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 def test_jina_new():
-    subprocess.run(["jina", "new", os.path.join(cur_dir, "hello-world")])
+    subprocess.run(['jina', 'new', os.path.join(cur_dir, 'hello-world')])
+    import docarray
 
-    assert os.path.exists(os.path.join(cur_dir, "hello-world"))
-    assert os.path.isdir(os.path.join(cur_dir, "hello-world"))
+    print(docarray.__version__)
+    subprocess.run(['pip', 'install', '-U', 'docarray', '-q'])
+    docarray = __import__('docarray')
+
+    print(docarray.__version__)
+    from docarray import DocList
+    from docarray.documents import TextDoc
+
+    assert os.path.exists(os.path.join(cur_dir, 'hello-world'))
+    assert os.path.isdir(os.path.join(cur_dir, 'hello-world'))
 
     with CloudFlow(path=os.path.join(cur_dir, "hello-world")) as flow:
         assert flow.endpoints != {}
@@ -23,8 +33,11 @@ def test_jina_new():
         assert 'gateway (websocket)' in flow.endpoints
         gateway = flow.endpoints['gateway (grpc)']
 
-        da = Client(host=gateway).post(on="/", inputs=DocumentArray.empty(2))
-        assert da.texts == ["hello, world!", "goodbye, world!"]
+        da = Client(host=gateway).post(
+            on='/', inputs=DocList[TextDoc](TextDoc() for i in range(2))
+        )
+        assert da[0].text == ['hello, world!']
+        assert da[1].text == ['goodbye, world!']
 
     shutil.rmtree(os.path.join(cur_dir, "hello-world"))
 

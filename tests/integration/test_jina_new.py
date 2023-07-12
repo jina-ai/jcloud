c3a6e44a7060889815e15a0b5751204f0ba8de04
Nikos Pitsillos <npitsillos@gmail.com>
2023-07-12 08:50:36 +0100

    fix: use venv in jina new test

--- tests/integration/test_jina_new.py
+++ tests/integration/test_jina_new.py
@@ -1,44 +1,47 @@
 import os
-import pip
+import sys
 import shutil
 import subprocess
+import tempfile
 
 import pytest
 
+from pathlib import Path
+from venv import create
+from jina import Client, DocumentArray
 from jcloud.flow import CloudFlow
-from jina import Client
 
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 
 
-def test_jina_new():
-    subprocess.run(['jina', 'new', os.path.join(cur_dir, 'hello-world')])
-    import docarray
+def setup_venv():
+    v_dir = Path(tempfile.mkdtemp())
+    create(v_dir, with_pip=True)
 
-    print(docarray.__version__)
-    subprocess.run(['pip', 'install', '-U', 'docarray', '-q'])
-    docarray = __import__('docarray')
+    _pip_path = v_dir / 'bin' / 'pip'
+    subprocess.run([_pip_path, 'install', '-U', 'pip', '-q'])
+    subprocess.run([_pip_path, 'install', 'jina[standard]==3.18.0', '-q'])
+    return v_dir
 
-    print(docarray.__version__)
-    from docarray import DocList
-    from docarray.documents import TextDoc
 
+def test_jina_new_project():
+    v_dir = setup_venv()
+    subprocess.run(
+        [v_dir / 'bin' / 'jina', 'new', os.path.join(cur_dir, 'hello-world')],
+    )
     assert os.path.exists(os.path.join(cur_dir, 'hello-world'))
     assert os.path.isdir(os.path.join(cur_dir, 'hello-world'))
 
-    with CloudFlow(path=os.path.join(cur_dir, "hello-world")) as flow:
+    with CloudFlow(path=os.path.join(cur_dir, 'hello-world')) as flow:
         assert flow.endpoints != {}
         assert 'gateway (grpc)' in flow.endpoints
         assert 'gateway (http)' in flow.endpoints
         assert 'gateway (websocket)' in flow.endpoints
         gateway = flow.endpoints['gateway (grpc)']
 
-        da = Client(host=gateway).post(
-            on='/', inputs=DocList[TextDoc](TextDoc() for i in range(2))
-        )
-        assert da[0].text == ['hello, world!']
-        assert da[1].text == ['goodbye, world!']
+        da = Client(host=gateway).post(on='/', inputs=DocumentArray.empty(2))
+        assert da.texts == ['hello, world!', 'goodbye, world!']
 
-    shutil.rmtree(os.path.join(cur_dir, "hello-world"))
+    shutil.rmtree(os.path.join(cur_dir, 'hello-world'))
 
-    assert not os.path.exists(os.path.join(cur_dir, "hello-world"))
+    assert not os.path.exists(os.path.join(cur_dir, 'hello-world'))

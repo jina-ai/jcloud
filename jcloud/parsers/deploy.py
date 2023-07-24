0fa57f4740d4901da7ebc97fd535911a445b6ad3
Nikos Pitsillos <npitsillos@gmail.com>
2023-07-24 19:44:28 +0100

    refactor: move deploy & normalize under flow resource

--- jcloud/parsers/deploy.py
+++ jcloud/parsers/deploy.py
@@ -1,12 +1,16 @@
-def set_deploy_parser(parser=None):
-    if not parser:
-        from .base import set_base_parser
+from .helper import _chf
 
-        parser = set_base_parser()
 
-    parser.add_argument(
+def set_deploy_parser(subparser):
+
+    deploy_parser = subparser.add_parser(
+        'deploy',
+        help='Deploy a Flow.',
+        formatter_class=_chf,
+    )
+
+    deploy_parser.add_argument(
         'path',
         type=str,
         help='The local path to a Jina flow project.',
     )
-    return parser
--- jcloud/parsers/normalize.py
+++ jcloud/parsers/normalize.py
@@ -1,27 +1,28 @@
 from pathlib import Path
+from .helper import _chf
 
 
-def set_normalize_parser(parser=None):
-    if not parser:
-        from .base import set_base_parser
-
-        parser = set_base_parser()
-    parser.add_argument(
+def set_normalize_parser(subparser):
+    normalize_parser = subparser.add_parser(
+        'normalize',
+        help='Normalize a Flow.',
+        formatter_class=_chf,
+    )
+    normalize_parser.add_argument(
         'path',
         type=Path,
         help='The local path to a Jina Flow project directory or yml file.',
     )
-    parser.add_argument(
+    normalize_parser.add_argument(
         '-o',
         '--output',
         type=Path,
         help='The output path to the normalized Jina Flow yml file.',
     )
-    parser.add_argument(
+    normalize_parser.add_argument(
         '-v',
         '--verbose',
         action='store_true',
         help='Increase verbosity.',
     )
-    parser.set_defaults(verbose=False)
-    return parser
+    normalize_parser.set_defaults(verbose=False)
